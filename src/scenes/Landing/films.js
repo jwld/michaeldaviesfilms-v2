68f2a2a46f25ac52537444006d03882de4c5de09
jwld <jackwldavies@icloud.com>
2020-01-12 10:25:39 +0000

    Add data file for Now page films

---
+++ src/scenes/Landing/films.js
@@ -0,0 +1,136 @@
+/* eslint-disable sort-keys */
+
+export default [
+  {
+    key: 'lafs',
+    vimeoId: '167906777',
+    title: 'Love At First Sight',
+    subtitle: 'Short Film',
+    blurb: 'Romantic comedy starring John Hurt and Phyllida Law.',
+    awards: {
+      wins: [
+        {
+          festival: 'Royal Television Society Award, UK 2010',
+          award: 'Best Short Film'
+        },
+        {
+          festival: 'Aspen Shortfest, USA 2011',
+          award: 'Audience Award'
+        },
+        {
+          festival: 'Dresden Internatinal Short Film Festival, Germany 2011',
+          award: 'Special Mention'
+        },
+        {
+          festival: 'British Independent Film Festival, UK 2011',
+          award: 'Best Director'
+        },
+        {
+          festival: 'British Independent Film Festival, UK 2011',
+          award: 'Best Actress'
+        },
+        {
+          festival: 'Rushes Soho Shorts Festival, UK 2011',
+          award: 'Best Short Film'
+        },
+        {
+          festival: 'Tri Media Film Festival, USA 2011',
+          award: 'Best International Film'
+        },
+        {
+          festival: 'Tri Media Film Festival, USA 2011',
+          award: 'Audience Award'
+        },
+        {
+          festival: 'Raindance Film Festival, UK 2011',
+          award: 'Best UK Short Film'
+        },
+        {
+          festival: 'Cornwall Film Festival, UK 2011',
+          award: 'Best International Short Film'
+        },
+        {
+          festival: 'Seattle Shorts Film Festival, USA 2011',
+          award: 'Best Actress'
+        },
+        {
+          festival: 'Prague Short Film Festival, Czech Republic 2012',
+          award: 'Audience Award'
+        },
+        {
+          festival: 'Postira Film Festival, Croatia 2012',
+          award: 'Audience Award'
+        },
+        {
+          festival: 'Ojai Film Festival, USA 2012',
+          award: 'Best Narrative Short'
+        },
+        {
+          festival: 'Rouen Film Festival, France 2012',
+          award: 'Best Fiction Film'
+        }
+      ],
+      nominations: [
+        {
+          festival: 'British Independent Film Awards, UK 2011',
+          award: 'Best British Short'
+        }
+      ],
+      shortlistings: [
+        {
+          festival: 'Academy Awards, USA 2012',
+          award: 'Live Action Short'
+        }
+      ]
+    }
+  },
+  {
+    key: 'wvm',
+    vimeoId: '167904928',
+    overlayInfo: '',
+    title: "What's Virgin Mean?",
+    subtitle: 'Short Film',
+    blurb: 'Comedy starring Kate Isitt.',
+    awards: {
+      wins: [
+        {
+          festival: 'Encounters Film Festival, UK 2008',
+          award: 'NFTS Special Mention'
+        },
+        {
+          festival: 'Prix de Court Pathe, Switzerland 2009',
+          award: 'Audience Award'
+        },
+        {
+          festival: 'CFF, USA 2009',
+          award: 'Special Jury Award For Best Comedy'
+        },
+        {
+          festival: 'Soul 4 Reel, USA 2010',
+          award: 'Audience Choice Award'
+        },
+        {
+          festival: 'Time Film Festival, Switzerland 2010',
+          award: 'Best Short Film'
+        },
+        {
+          festival: 'Bratislava International Film Festival, Slovakia 2010',
+          award: 'Best Short Film'
+        },
+        {
+          festival: 'Forster Film Festival, Australia 2013',
+          award: 'Originality Award'
+        }
+      ]
+    }
+  },
+  {
+    key: 'mm',
+    vimeoId: '352929055',
+    overlayInfo: '',
+    title: 'Mirror Mirror',
+    subtitle: 'Short Film',
+    blurb: 'RADA Film. Romantic comedy.',
+    awards: {}
+  }
+]
