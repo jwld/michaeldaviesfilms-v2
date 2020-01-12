afa94e444ab844706a836a8ad6910630a064055d
jwld <jackwldavies@icloud.com>
2020-01-12 11:13:44 +0000

    Render film tiles on now page

--- src/scenes/Landing/index.jsx
+++ src/scenes/Landing/index.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
+import films from './films'
 import * as SC from './style'
 
-const Landing = () => <SC.Landing>NOW</SC.Landing>
+const Landing = () => (
+  <SC.Landing>
+    <SC.FilmsWrap>
+      {films.map(film => (
+        <SC.FilmTile key={film.key}>
+          <SC.Film image={film.key}></SC.Film>
+        </SC.FilmTile>
+      ))}
+    </SC.FilmsWrap>
+  </SC.Landing>
+)
 
 export default Landing
--- src/scenes/Landing/style.js
+++ src/scenes/Landing/style.js
@@ -1,5 +1,30 @@
+import { cover } from 'polished'
 import styled from 'styled-components'
 
-import { components } from 'style'
+import { components, mixins } from 'style'
 
 export const Landing = styled(components.PageWrap)``
+
+export const FilmsWrap = styled.div`
+  display: grid;
+  grid-gap: 1rem;
+  grid-template-columns: repeat(auto-fill, minmax(35rem, 1fr));
+`
+
+export const FilmTile = styled.div`
+  padding-bottom: 56.25%;
+  position: relative;
+`
+
+export const Film = styled.div`
+  ${cover()};
+  ${mixins.flex};
+
+  background: pink;
+  background-image: url('images/${props => props.image}.png');
+  background-position: center;
+  background-size: cover;
+  border-radius: 0.3rem;
+  box-shadow: 0 0.8rem 1rem -0.5rem black;
+  cursor: pointer;
+`
