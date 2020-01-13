f18dd34ac748259468323b6896f78946cd5f6d07
jwld <jackwldavies@icloud.com>
2020-01-13 04:07:28 +0000

    Add media queries

--- src/style/index.js
+++ src/style/index.js
@@ -1,3 +1,4 @@
 export * as components from './components'
+export * as media from './media'
 export * as mixins from './mixins'
 export * as vars from './variables'
---
+++ src/style/media.js
@@ -0,0 +1,1 @@
+export const tablet = '@media (max-width: 1100px)'
