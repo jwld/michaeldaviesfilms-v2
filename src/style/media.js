c2749306f3b3628011d6b4ce491b86be29b3df28
jwld <jackwldavies@icloud.com>
2020-01-13 18:40:58 +0000

    Add 700px media query

--- src/style/media.js
+++ src/style/media.js
@@ -1,3 +1,3 @@
 export const mobile = '@media (max-width: 600px)'
-export const mobileBig = '@media (max-width: 650px)'
+export const m700 = '@media (max-width: 700px)'
 export const tablet = '@media (max-width: 1000px)'
