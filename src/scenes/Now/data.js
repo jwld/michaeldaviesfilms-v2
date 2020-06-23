118c118ee3b40b7957304051473128b95e85dc35
jwld <jackwldavies@icloud.com>
2020-06-23 18:11:37 +0100

    Swap lafs and wvm positions

--- src/scenes/Now/data.js
+++ src/scenes/Now/data.js
@@ -1,10 +1,59 @@
 export const text = [
-  "Michael Davies is a BAFTA and RTS award-winning writer and director, currently in pre-production with his first feature, <i>The Captive Heart</i>, a romantic adventure set in Italy in the 1860's and based on a true story. Carlo Cresto-Dina at Tempesta is producing and filming is (re)scheduled for autumn 2021, on the Amalfi Coast.",
+  "Michael Davies is a BAFTA and RTS award-winning writer and director, currently in pre-production with his first feature, <i>The Captive Heart</i>, a romantic adventure set in Italy in the 1860's and based on a true story. Carlo Cresto-Dina at Tempesta is producing and filming is (re)scheduled for autumn 202, on the Amalfi Coast.",
   "Michael frequently works with RADA. In 2019 he directed the controversial sell-out play, <i>Safe Space</i>, at the RADA Festival, the short film <i>Mirror Mirror</i>, and rehearsed readings of other new work. The script for <i>The Captive Heart</i>, and for Michael's paranormal romantic comedy, <i>Psychopomp</i>, (supported by the Wellcome Trust) have both been developed with RADA.",
   "Michael's short film <i>What's Virgin Mean?</i> &nbsp;has screened at over sixty international festivals, winning eight awards and attracting more than ten million hits on YouTube. Unusually for a short, it also made a profit. His next film, <i>Love At First Sight</i>, starring Phyllida Law and John Hurt, won fifteen awards, including an RTS, Rushes and Raindance, and was shortlisted for an Oscar."
 ]
 
 export const films = [
+  {
+    awards: [
+      {
+        award: 'NFTS Special Mention',
+        festival: 'Encounters Film Festival',
+        result: 'winner'
+      },
+      {
+        award: 'Audience Award',
+        festival: 'Prix de Court Pathe',
+        result: 'winner'
+      },
+      {
+        award: 'Special Jury Award For Best Comedy',
+        festival: 'CFF',
+        result: 'winner'
+      },
+      {
+        award: 'Audience Choice Award',
+        festival: 'Soul 4 Reel',
+        result: 'winner'
+      },
+      {
+        award: 'Best Short Film',
+        festival: 'Time Film Festival',
+        result: 'winner'
+      },
+      {
+        award: 'Best Short Film',
+        festival: 'Bratislava International Film Festival',
+        result: 'winner'
+      },
+      {
+        award: 'Originality Award',
+        festival: 'Forster Film Festival',
+        result: 'winner'
+      },
+      {
+        award: 'First Prize (The Twisted Bun Award)',
+        festival: 'Sodertljie Food Film Festival',
+        result: 'winner'
+      }
+    ],
+    description: 'Comedy starring Kate Isitt.',
+    key: 'wvm',
+    title: "What's Virgin Mean?",
+    type: 'Short Film',
+    vimeoId: '167904928'
+  },
   {
     awards: [
       {
@@ -99,55 +148,6 @@ export const films = [
     title: 'Love At First Sight',
     type: 'Short Film',
     vimeoId: '167906777'
-  },
-  {
-    awards: [
-      {
-        award: 'NFTS Special Mention',
-        festival: 'Encounters Film Festival',
-        result: 'winner'
-      },
-      {
-        award: 'Audience Award',
-        festival: 'Prix de Court Pathe',
-        result: 'winner'
-      },
-      {
-        award: 'Special Jury Award For Best Comedy',
-        festival: 'CFF',
-        result: 'winner'
-      },
-      {
-        award: 'Audience Choice Award',
-        festival: 'Soul 4 Reel',
-        result: 'winner'
-      },
-      {
-        award: 'Best Short Film',
-        festival: 'Time Film Festival',
-        result: 'winner'
-      },
-      {
-        award: 'Best Short Film',
-        festival: 'Bratislava International Film Festival',
-        result: 'winner'
-      },
-      {
-        award: 'Originality Award',
-        festival: 'Forster Film Festival',
-        result: 'winner'
-      },
-      {
-        award: 'First Prize (The Twisted Bun Award)',
-        festival: 'Sodertljie Food Film Festival',
-        result: 'winner'
-      }
-    ],
-    description: 'Comedy starring Kate Isitt.',
-    key: 'wvm',
-    title: "What's Virgin Mean?",
-    type: 'Short Film',
-    vimeoId: '167904928'
   },
   {
     description:
